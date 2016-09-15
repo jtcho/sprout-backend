75db5a8185dae0a32f50badd3009de66e5c58f6c
JT Cho <jonathan.t.cho@gmail.com>
2016-09-15 15:19:14 -0400

    Temporary rollback of database code to ramp Igor up.

--- src/edu/upenn/sprout/controllers/AccountController.java
+++ src/edu/upenn/sprout/controllers/AccountController.java
@@ -1,45 +1,48 @@
-package edu.upenn.sprout.controllers;
-
-import edu.upenn.sprout.db.Account;
-import play.db.jpa.JPA;
-import play.db.jpa.Transactional;
-import play.mvc.Controller;
-import play.mvc.Result;
-
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-
-/**
- */
-public class AccountController extends Controller {
-
-  public AccountController() {
-  }
-
-  @Transactional
-  public Result createAccount() {
-    String email = request().body().asJson().get("email").asText();
-    try {
-      Account account = new Account(email);
-      EntityManager em = JPA.em();
-      em.persist(account);
+//package edu.upenn.sprout.controllers;
+//
+//import edu.upenn.sprout.db.Account;
+//import play.db.jpa.JPA;
+//import play.db.jpa.Transactional;
+//import play.mvc.Controller;
+//import play.mvc.Result;
+//
+//import javax.inject.Inject;
+//import javax.persistence.EntityManager;
+//
+///**
+// */
+//public class AccountController extends Controller {
+//
+//  public AccountController() {
+//  }
+//
+//  @Transactional
+//  public Result createAccount() {
+//    String email = request().body().asJson().get("email").asText();
+//    try {
+//      Account account = new Account(email);
+//      EntityManager em = JPA.em();
+//      em.persist(account);
 //      em.flush();
-      return created(account.toString());
-    } catch (Exception e) {
-      System.err.println("Received internal server error with message: " + e);
-      return internalServerError(e.toString());
-    }
-  }
-
-  @Transactional
-  public Result getAccount(Long id) {
-    try {
-      Account account = JPA.em().find(Account.class, id);
-      return ok(account.toString());
-    } catch (Exception e) {
-      System.err.println("Received internal server error with message: " + e);
-      return internalServerError(e.toString());
-    }
-  }
-
-}
+//      return created(account.toString());
+//    } catch (Exception e) {
+//      System.err.println("Received internal server error with message: " + e);
+//      return internalServerError(e.toString());
+//    }
+//  }
+//
+//  @Transactional
+//  public Result getAccount(Long id) {
+//    try {
+//      Account account = JPA.em().find(Account.class, id);
+//      if (account == null) {
+//        return notFound("Entity was not found.");
+//      }
+//      return ok(account.toString());
+//    } catch (Exception e) {
+//      System.err.println("Received internal server error with message: " + e);
+//      return internalServerError(e.toString());
+//    }
+//  }
+//
+//}
--- src/edu/upenn/sprout/db/Account.java
+++ src/edu/upenn/sprout/db/Account.java
@@ -1,63 +1,62 @@
-package edu.upenn.sprout.db;
-
-import org.hibernate.annotations.GenericGenerator;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import java.sql.Date;
-
-/**
- * @author jtcho
- */
-@Entity
-@Table(name = "Accounts")
-public class Account {
-
-  private Long id;
-  private String email;
-  private Date createdOn;
-  private String content;
-
-  public Account() {}
-
-  public Account(String email) {
-    this.email = email;
-    this.createdOn = new Date(System.currentTimeMillis());
-  }
-
-  @Id
-  @GeneratedValue(generator="increment")
-  @GenericGenerator(name="increment", strategy="increment")
-  public Long getId() {
-    return id;
-  }
-
-  protected void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getEmail() {
-    return this.email;
-  }
-
-  protected void setEmail(String email) {
-    this.email = email;
-  }
-
-
-  public Date getCreatedOn() {
-    return this.createdOn;
-  }
-
-  protected void setCreatedOn(Date createdOn) {
-    this.createdOn = createdOn;
-  }
-
-  public String toString() {
-    return new StringBuilder("Account[").append(id).append(", ").append(email).append(", ").append(createdOn).append("]").toString();
-
-  }
-
-}
+//package edu.upenn.sprout.db;
+//
+//import org.hibernate.annotations.GenericGenerator;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//import java.sql.Date;
+//
+///**
+// * @author jtcho
+// */
+//@Entity
+//@Table(name = "Accounts")
+//public class Account {
+//
+//  private Long id;
+//  private String email;
+//  private Date createdOn;
+//
+//  public Account() {}
+//
+//  public Account(String email) {
+//    this.email = email;
+//    this.createdOn = new Date(System.currentTimeMillis());
+//  }
+//
+//  @Id
+//  @GeneratedValue(generator="increment")
+//  @GenericGenerator(name="increment", strategy="increment")
+//  public Long getId() {
+//    return id;
+//  }
+//
+//  protected void setId(Long id) {
+//    this.id = id;
+//  }
+//
+//  public String getEmail() {
+//    return this.email;
+//  }
+//
+//  protected void setEmail(String email) {
+//    this.email = email;
+//  }
+//
+//
+//  public Date getCreatedOn() {
+//    return this.createdOn;
+//  }
+//
+//  protected void setCreatedOn(Date createdOn) {
+//    this.createdOn = createdOn;
+//  }
+//
+//  public String toString() {
+//    return new StringBuilder("Account[").append(id).append(", ").append(email).append(", ").append(createdOn).append("]").toString();
+//
+//  }
+//
+//}
